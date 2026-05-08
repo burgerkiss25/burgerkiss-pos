8010b8ac9c4a481839c741bf1be576a6184c0670
burgerkiss25 <burgerkiss25@gmail.com>
2026-05-08 01:17:48 +0000

    Start phase 2 online image sync

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// Produktions-/Entwicklungs-Konfiguration für Online-DB.
+// Diese Datei wird von index.html geladen und aktiviert Firebase Realtime Database.
+
+window.BK_ALLOW_PRICE_EDIT = true;
+window.BK_SYNC_ENABLED = true;
+window.BK_SYNC_FORCE_SLOT = 'SN1';
+window.BK_SYNC_PATH = '/pos/live';
+window.BK_SYNC_INTERVAL_MS = 1200;
+window.BK_IMAGES_PATH = '/pos/config/images';
+
+// Firebase Web-App-Konfiguration aus der Firebase Console.
+// Hinweis: Keine `import`-Zeilen hier einfügen; die App nutzt Firebase Compat-Skripte aus index.html.
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyB10r5qCm6wbJ7Cosfm2aztsxKzCCUKhfY',
+  authDomain: 'burgerkiss-pos-system.firebaseapp.com',
+  databaseURL: 'https://burgerkiss-pos-system-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'burgerkiss-pos-system',
+  storageBucket: 'burgerkiss-pos-system.firebasestorage.app',
+  messagingSenderId: '469356847680',
+  appId: '1:469356847680:web:c5fc5baa3a4fe36861b5a5',
+  measurementId: 'G-4PKG8C8ERM'
+};
